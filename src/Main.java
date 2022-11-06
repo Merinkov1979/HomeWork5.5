9191cf25215b4d699a43a9dc78028bafa9747b19
Привет <mer79@internet.ru>
2022-11-06 19:17:02 +0300

    Коммит2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
+
 public class Main {
     public static void main(String[] args) {
+        boolean result = validator.check(" test_1234", " D_1hWiKjjP_9 ", " D_1hWiKjjP_9 " );
+        if (result){
+            System.out.println("Логин и пароль корректные");
+        } else {
+            System.out.println("Логин и/или пароль некорректный");
+        }
 
     }
 }
